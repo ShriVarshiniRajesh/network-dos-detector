7dc2f68456adfced5abef4bb423123747d8744bb
ShriVarshiniRajesh <bala.varshini1977@gmail.com>
2026-06-16 13:36:33 +0530

    Python files uploaded

---
+++ firewall_dos.py
@@ -0,0 +1,44 @@
+import os
+import sys
+import ctypes
+import time
+from collections import defaultdict
+from scapy.all import sniff, IP
+
+THRESHOLD = 40
+print(f"THRESHOLD: {THRESHOLD}")
+
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+def packet_callback(packet):
+    src_ip = packet[IP].src
+    packet_count[src_ip] += 1
+    current_time = time.time()
+    time_interval = current_time - start_time[0]
+
+    if time_interval >= 1:
+        for ip, count in packet_count.items():
+            packet_rate = count / time_interval
+
+            if packet_rate > THRESHOLD and ip not in flagged_ips:
+                print(f"[ALERT] Possible DoS detected from {ip} — packet rate: {packet_rate:.2f} pkts/sec")
+                flagged_ips.add(ip)
+
+        packet_count.clear()
+        start_time[0] = current_time
+
+if __name__ == "__main__":
+    if not is_admin():
+        print("This script requires administrator privileges.")
+        sys.exit(1)
+
+    packet_count = defaultdict(int)
+    start_time = [time.time()]
+    flagged_ips = set()
+
+    print("Monitoring network traffic...")
+    sniff(filter="ip", prn=packet_callback)
